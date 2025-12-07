726966554ba991192cb03b9438c7a6ad9d684f81
VNcerti <tlvdzreal@gmail.com>
2025-12-07 12:06:41 +0700

    Update and rename google-analytics.js to config.js

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// Configuration variables
+const CONFIG = {
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby_-MluMh6fh7jl0Ai2iy6ojqX4UhjhjxRWj8RV8wRgNPUJHd6TphKwm6yPtYPm8hsCwg/exec',
+    CACHE_KEY: 'xspace_apps_cache',
+    CACHE_TIMESTAMP_KEY: 'xspace_cache_timestamp',
+    CACHE_DURATION: 30 * 60 * 1000, // 30 phút
+    CATEGORY_LABELS: {
+        'game': 'Trò chơi',
+        'social': 'Mạng xã hội',
+        'entertainment': 'Giải trí',
+        'photo': 'Chụp ảnh',
+        'clone': 'Nhân bản',
+        'premium': 'Mở khoá Premium'
+    },
+    
+    // Thêm các URL khác vào đây
+    VIETQR_BASE_URL: 'https://img.vietqr.io/image/mbbank-311435-compact2.png'
+};
